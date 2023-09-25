fa17c0ef1f7075857e0e86c7432c92035d5a7d45
La Minh Duc <laminhduc200@gmail.com>
2023-09-26 01:49:34 +0700

    add the function to create a file for users to enter answers

--- khkt.cpp
+++ khkt.cpp
@@ -7,8 +7,15 @@ int main()
     SetConsoleOutputCP(65001);
 #endif
     /*--------------------------Hướng dẫn---------------------------------------*/
-    cout << "Chào mừng tới chương trình của Vịt ngoo\n";
-    cout << "Vịt chỉ muốn nói là tớ yêu bé Dipz nhìu lém ó\n";
+    fstream guide("Input/guide.txt", ios::in);
+    string guide_script;
+    while (!guide.eof())
+    {
+        getline(guide, guide_script, '\n');
+        cout << guide_script << '\n';
+        guide_script = "";
+    }
+    guide.close();
     system("pause");
     /*--------------------------------------------------------------------------*/
 
@@ -45,9 +52,15 @@ int main()
         vocab_test(source_vocab, idx_select[i], ID);
     }
     file_name = "Output/participant/" + string(3 - to_string(ID).length(), '0') + to_string(ID) + ".txt";
-    result.open(file_name, ios::out);
+    result.open(file_name, ios::app);
     result << "\nResult: " << correct << "/" << NUM_Q << endl;
     result << "Correct: " << correct << "\tWrong: " << NUM_Q - correct << endl;
+    result.close();
+    /*--------------------------------------------------------------------------*/
+
+    /*Tạo file để người dùng nhập từ vựng*/
+    result.open("vocab_answer.txt", ios::out);
+    result << "ID: " << string(3 - to_string(ID).length(), '0') + to_string(ID) << "\n\n";
     result.close();
     /*--------------------------------------------------------------------------*/
 
@@ -58,21 +71,9 @@ int main()
     /*Lời cảm ơn*/
     cout << "Cảm ơn đã tham gia khảo sát\n";
     /*--------------------------------------------------------------------------*/
-#ifdef DEBUG
-    cout
-        << "debug choose\n";
-    for (int i = 0; i < 20; i++)
-    {
-        cout << select_equation[i] << " ";
-    }
-    cout << endl;
-    // cout << source[0].first << " " << source[0].second << endl;
-#endif
-
-    /*Cập nhật lại ID cho người dùng kết tiếp*/
-    /*--------------------------------------------------------------------------*/
 
     cout << "Press any key to finish . . .";
+    getchar();
     getchar();
     return 0;
 }
