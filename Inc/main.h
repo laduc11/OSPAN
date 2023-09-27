65734635f10ff0320936f9dca9632dd14de16031
La Minh Duc <laminhduc200@gmail.com>
2023-09-27 18:26:01 +0700

    add create summary report module

--- Inc/main.h
+++ Inc/main.h
@@ -19,8 +19,8 @@ using namespace std;
 // #define DEBUG
 #define TIENG_VIET
 #define NUM_Q 20
-#define DELAY_TIMER_M 100  // Math: 6s
-#define DELAY_TIMER_L 3000 // Memory ability: 3s
+#define DELAY_TIMER_M 100 // Math: 6s
+#define DELAY_TIMER_L 100 // Memory ability: 3s
 #define DELAY_TIMER_S 500
 #define TOTAL_Q 100
 #define MAX_FILE 999
